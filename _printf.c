4b3a6b55fce2c3cef201196fcdbf6b1877447c44
Tembeka <thembeka.zulu96@gmail.com>
2023-06-30 17:15:20 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -11,8 +11,8 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, c = va_arg(args, int);
-	const char *ptr = format, *s = va_arg(args, const char *);
+	int count = 0;
+	const char *ptr = format;
 
 	va_start(args, format);
 	if (format == NULL)
@@ -24,11 +24,14 @@ int _printf(const char *format, ...)
 			ptr++;
 			if (*ptr == 'c')
 			{
+				int c = va_arg(args, int);
+
 				putchar(c);
 				count++;
 			}
 			else if (*ptr == 's')
 			{
+				const char *s = va_arg(args, const char *);
 				while (*s != '\0')
 				{
 					putchar(*s);
